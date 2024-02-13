e4418eafdc873f3cbf0f838a6a052268181a579d
vd <33198864+vd2org@users.noreply.github.com>
2024-02-14 01:11:36 +0400

    Initial commit

---
+++ whannel.py
@@ -0,0 +1,338 @@
+# "client" and "server"
+
+import asyncio
+import logging
+import signal
+import sys
+from argparse import ArgumentParser
+from asyncio import StreamReader, StreamWriter, create_task, Task, Event
+from contextlib import suppress
+from typing import Tuple, Optional, Awaitable, Callable
+
+from websockets import WebSocketCommonProtocol, ConnectionClosed
+from websockets import client as ws
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger = logging.getLogger("whannel")
+logger.setLevel(logging.DEBUG if sys.flags.debug else logging.INFO)
+
+CONNECTION_TIMEOUT = 30
+TOUCH_TIMEOUT = 50
+READ_SIZE = 1024
+
+
+async def open_data_connection(url: str) -> WebSocketCommonProtocol:
+    async with asyncio.timeout(CONNECTION_TIMEOUT):
+        try:
+            logger.debug("Trying to open data connection...")
+            return await ws.connect(url)
+        except Exception as e:
+            logger.error(f"Failed to open data connection: {e}")
+            await asyncio.sleep(1)
+
+
+def rw_factory(client_reader: StreamReader, client_writer: StreamWriter, ws: WebSocketCommonProtocol) -> Tuple[Task, Task]:
+    async def reader():
+        while not client_reader.at_eof():
+            data = await client_reader.read(READ_SIZE)
+            print("READ", data)
+            await ws.send(data)
+
+    async def writer():
+        while not ws.closed:
+            try:
+                data = await asyncio.wait_for(ws.recv(), timeout=TOUCH_TIMEOUT)
+                print("WRITE", data)
+                if not data:
+                    raise TimeoutError
+                client_writer.write(data)
+            except TimeoutError:
+                await ws.send(b"")
+
+    return asyncio.create_task(reader()), asyncio.create_task(writer())
+
+
+async def accept_connection(host: str, port: int, url: str, gate_id: str, connection_id: str, connection_secret: str):
+    client_writer: Optional[StreamWriter] = None
+    reader: Optional[Task] = None
+    writer: Optional[Task] = None
+    connection = None
+
+    try:
+        connection_url = f"{url}connect/{gate_id}/acceptor/{connection_id}/{connection_secret}"
+
+        connection = await open_data_connection(connection_url)
+        logger.info(f"Established a new data connection {connection_id}!")
+
+        logger.debug("Connecting to target... ")
+
+        client_reader, client_writer = await asyncio.open_connection(host, port)
+        logger.info(f"Connected to target server {host}:{port}!")
+        logger.info(f"Serving connection!")
+
+        # Working with connection
+        reader, writer = rw_factory(client_reader, client_writer, connection)
+        await asyncio.wait((reader, writer), return_when=asyncio.FIRST_COMPLETED)
+        print("END!!!!")
+    except (TimeoutError, ConnectionError, ConnectionClosed) as e:
+        logger.error(f"Broken data connection: {e}")
+        logger.exception(f"Broken data connection: {e}")
+    except Exception as e:
+        logger.error(f"Unexpected error in data connection: {e}")
+    finally:
+        if reader:
+            reader.cancel()
+            with suppress(BaseException):
+                await reader
+        if writer:
+            writer.cancel()
+            with suppress(BaseException):
+                await writer
+        if client_writer is not None:
+            with suppress():
+                client_writer.close()
+        if connection is not None:
+            with suppress():
+                await connection.close()
+
+        logger.info(f"Finished connection {connection_id}!")
+
+
+async def process_connection(stop_event: Event, control: WebSocketCommonProtocol,
+                             client_reader: StreamReader, client_writer: StreamWriter,
+                             url: str, gate_id: str):
+    connection: Optional[WebSocketCommonProtocol] = None
+    reader: Optional[Task] = None
+    writer: Optional[Task] = None
+    connection_id = None
+
+    try:
+        await control.send("CONNECT")
+        while True:
+            connection_rep = await asyncio.wait_for(control.recv(), CONNECTION_TIMEOUT)
+            if connection_rep != "TOUCH":
+                break
+        print(connection_rep)
+        rep, connection_id, secret = connection_rep.split(" ")
+
+        if rep != "CONNECTION":
+            raise ConnectionError(f"Wrong response: {connection_rep}")
+    except (TimeoutError, ConnectionError, ConnectionClosed, ValueError) as e:
+        print("SET STOP EVENT")
+        stop_event.set()
+        logger.error(f"Control connection is broken: {e}")
+        return
+    except Exception as e:
+        print("SET STOP EVENT")
+        stop_event.set()
+        logger.error(f"Unexpected error in control connection: {type(e)}: {e}")
+        return
+
+    try:
+        connection = await open_data_connection(f"{url}connect/{gate_id}/requestor/{connection_id}/{secret}")
+        reader, writer = rw_factory(client_reader, client_writer, connection)
+        await asyncio.wait((reader, writer), return_when=asyncio.FIRST_COMPLETED)
+    except (TimeoutError, ConnectionError, ConnectionClosed) as e:
+        logger.error(f"Broken data connection: {e}")
+    except Exception as e:
+        logger.error(f"Unexpected error in data connection: {type(e)}: {e}")
+    finally:
+        if reader:
+            reader.cancel()
+            with suppress(BaseException):
+                await reader
+        if writer:
+            writer.cancel()
+            with suppress(BaseException):
+                await writer
+        with suppress():
+            client_writer.close()
+        if connection is not None:
+            if connection:
+                with suppress():
+                    await connection.close()
+
+        if connection_id:
+            logger.info(f"Finished connection {connection_id}!")
+        else:
+            logger.info(f"Finished unknown connection!")
+
+
+async def tcp_requestor(url: str, host: str, port: int, gate: str):
+    gate_id = gate
+    control_url = f"{url}associate/{gate_id}"
+
+    logger.debug(f"Connecting to {control_url}...")
+
+    stop_event = asyncio.Event()
+
+    control = None
+    srv = None
+    connections = []
+
+    try:
+        # Opening control connection
+        async with asyncio.timeout(CONNECTION_TIMEOUT):
+            control = await ws.connect(control_url)
+            if await control.recv() != "OK":
+                raise ConnectionError(f"Failed to connect to gate: {gate_id}")
+
+        logger.info(f"Connected to gate {gate_id}!")
+
+        def _process(client_reader: StreamReader, client_writer: StreamWriter):
+            connection = asyncio.create_task(process_connection(stop_event, control, client_reader, client_writer, url, gate_id))
+            connections.append(connection)
+            return connection
+
+        srv = await asyncio.start_server(_process, host=host, port=port)
+
+        while True:
+            try:
+                await asyncio.wait_for(stop_event.wait(), TOUCH_TIMEOUT)
+                print("STOPPING EVENT!!!!")
+                return
+            except TimeoutError:
+                await control.send("TOUCH")
+    except (TimeoutError, ConnectionError, ConnectionClosed) as e:
+        logger.error(f"Broken connection: {e}")
+    except Exception as e:
+        logger.error(f"Unexpected error: {type(e)}: {e}")
+    finally:
+        if srv:
+            with suppress():
+                srv.close()
+        if control:
+            with suppress():
+                await control.close()
+        for conn in connections:
+            conn.cancel()
+            with suppress(BaseException):
+                await conn
+
+        print("FINISHED!!!!")
+
+
+async def tcp_acceptor(url: str, host: str, port: int, secret: str):
+    control_url = f"{url}create/{secret}"
+
+    logger.debug(f"Connecting to {control_url}...")
+
+    control = None
+    connections = []
+
+    try:
+        # Opening control connection
+        async with asyncio.timeout(CONNECTION_TIMEOUT):
+            control = await ws.connect(control_url)
+            msg = await control.recv()
+            ok, gate_id = msg.split(" ")
+
+            if ok != "OK":
+                raise ConnectionError(f"Failed to create a gate: {gate_id}")
+
+        logger.info(f"Created new gate {gate_id}!")
+
+        while True:
+            try:
+                connect_req = await asyncio.wait_for(control.recv(), TOUCH_TIMEOUT)
+
+                req, *params = connect_req.split(" ")
+
+                if req == "TOUCH":
+                    raise TimeoutError
+
+                if req != "CONNECTION":
+                    raise ConnectionError(f"Invalid request: {connect_req}")
+
+                connection_id, connection_secret = params
+
+                conn = create_task(accept_connection(host, port, url, gate_id, connection_id, connection_secret))
+                connections.append(conn)
+            except TimeoutError:
+                await control.send("TOUCH")
+
+                # Cleaning up broken connections
+                for conn in list(connections):
+                    if conn.done():
+                        connections.remove(conn)
+                        with suppress():
+                            await conn
+
+    except (TimeoutError, ConnectionError, ConnectionClosed) as e:
+        logger.error(f"Broken control connection: {e}")
+    except Exception as e:
+        logger.error(f"Unexpected error in control connection: {type(e)}: {e}")
+    finally:
+        print(1)
+        with suppress():
+            await control.close()
+        print(2)
+        for conn in connections:
+            conn.cancel()
+            with suppress(BaseException):
+                await conn
+        print(3)
+
+
+async def runner(worker: Callable[..., Awaitable], *args, **kwargs):
+    stop = asyncio.Event()
+
+    loop = asyncio.get_event_loop()
+    loop.add_signal_handler(signal.SIGINT, lambda: stop.set())
+
+    task = asyncio.create_task(worker(*args, **kwargs))
+    wait = asyncio.create_task(stop.wait())
+
+    await asyncio.wait((task, wait), return_when=asyncio.FIRST_COMPLETED)
+
+    wait.cancel()
+    task.cancel()
+
+    try:
+        await task
+    except BaseException:
+        pass
+
+    logger.info("Bye!")
+
+
+def main():
+    parser = ArgumentParser(prog="whannel", description="The websocket connections multiplexor gateway client.")
+    parser.add_argument("url", help="Gateway connection URL.")
+
+    suppress = parser.add_subparsers(title="Working mode", metavar="mode", required=True, dest="mode")
+
+    requestor = suppress.add_parser("requestor", help="The requestor mode.")
+    requestor.add_argument("gate", help="Gateway id.")
+    requestor.add_argument("port", help="Port to listen.")
+    requestor.add_argument("-n", "--host", default="localhost", help="Hostname/ip to listen.")
+
+    acceptor = suppress.add_parser("acceptor", help="The acceptor mode.")
+    acceptor.add_argument("host", help="Hostname/ip to connect to.")
+    acceptor.add_argument("port", help="Port to connect to.")
+    acceptor.add_argument("-s", "--secret", default="gate", help="The secret to use for authentication.")
+
+    # sock_requestor = suppress.add_parser("sock-requestor", help="The sock requestor mode.")
+    # sock_requestor.add_argument("gate", help="Gateway id.")
+    # sock_requestor.add_argument("path", default="./connect.sock", help="Path to listen socket file.")
+    #
+    # sock_acceptor = suppress.add_parser("sock-acceptor", help="The sock acceptor mode.")
+    # sock_acceptor.add_argument("path", help="Path to socket file to connect to.")
+
+    args = vars(parser.parse_args())
+
+    workers = {
+        "requestor": tcp_requestor,
+        "acceptor": tcp_acceptor,
+        # "sock-requestor": sock_requestor,
+        # "sock-acceptor": sock_acceptor,
+    }
+
+    if not (worker := workers.get(args.pop("mode"))):
+        raise NotImplementedError(f"Mode is not implemented!")
+
+    asyncio.run(runner(worker, **args))
+
+
+if __name__ == "__main__":
+    main()
---
+++ wultiplexor.py
@@ -0,0 +1,339 @@
+import asyncio
+import dataclasses
+import logging
+import secrets
+import signal
+import string
+import sys
+import time
+from argparse import ArgumentParser
+from contextlib import suppress
+from typing import Dict, Optional
+
+from websockets import serve, WebSocketServerProtocol, ConnectionClosed
+from websockets.frames import CloseCode
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger = logging.getLogger("wultiplexor")
+logger.setLevel(logging.DEBUG if sys.flags.debug else logging.INFO)
+
+CONNECTION_TIMEOUT = 300  # TODO!!!
+TOUCH_TIMEOUT = 5
+TOKEN_LENGTH = getattr(secrets, "DEFAULT_ENTROPY", 32)
+
+
+class AuthenticationError(Exception):
+    """Raised when the client fails to authenticate."""
+
+
+class AlreadyEstablishedConnectionError(Exception):
+    """Raised when the client fails to authenticate."""
+
+
+@dataclasses.dataclass
+class Connection:
+    id: str
+    created_at: int
+    associated_id: str
+    requestor_secret: str
+    acceptor_secret: str
+    requestor: Optional[WebSocketServerProtocol]
+    acceptor: Optional[WebSocketServerProtocol]
+
+
+@dataclasses.dataclass
+class Gate:
+    id: str
+    control: WebSocketServerProtocol
+    associated: Dict[str, WebSocketServerProtocol]
+    connections: Dict[str, Connection]
+
+
+class GatesProvider:
+    def __init__(self, secret: str, single: Optional[str] = None):
+        self._gates: Dict[str, Gate] = {}
+        self._secret: str = secret
+        self._single: Optional[str] = single
+
+    async def auth(self, ws, secret: str):
+        """Authenticates the client."""
+
+        if not secrets.compare_digest(secret, self._secret):
+            raise AuthenticationError
+
+    @staticmethod
+    def generate_gate_id(groups: int = 4, group_size: int = 4, alphabet: str = string.ascii_lowercase, separator: str = "_") -> str:
+        """Generates a new human-readable gate id."""
+
+        return separator.join(str().join(secrets.choice(alphabet) for _ in range(group_size)) for _ in range(groups))
+
+    @staticmethod
+    def remote_addr(ws: WebSocketServerProtocol) -> str:
+        """Formats the remote address of the client."""
+        return ":".join(map(str, ws.remote_address))
+
+    async def _create(self, ws: WebSocketServerProtocol):
+        """Creates a new gateway connection."""
+
+        if self._single and self._single in self._gates:
+            logger.warning(f"[{self.remote_addr(ws)}] In single mode, closing the existing gate...")
+            eg = self._gates.pop(self._single)
+            with suppress():
+                await eg.control.close()
+
+        if self._single:
+            gate_id = self._single
+        else:
+            while (gate_id := self.generate_gate_id()) in self._gates:
+                pass
+
+        self._gates[gate_id] = gate = Gate(id=gate_id, control=ws, associated={}, connections={})
+
+        logger.info(f"[{self.remote_addr(ws)}] Creating a new gate {gate_id}...")
+
+        try:
+            await ws.send(f"OK {gate_id}")
+
+            # Holding the connection: receiving data, ignoring it and sending a touch
+            while True:
+                try:
+                    await asyncio.wait_for(ws.recv(), timeout=TOUCH_TIMEOUT)
+                except TimeoutError:
+                    logger.debug(f"[{self.remote_addr(ws)}] Idle gateway connection {gate_id}...")
+                    await ws.send("TOUCH")
+                    continue
+
+                # TODO: TBDL: Cleanup idle connections for the gateway!
+
+        except ConnectionClosed:
+            logger.info(f"[{self.remote_addr(ws)}] Gateway {gate_id} connection is closed, cleaning up...")
+        except Exception as e:
+            logger.debug(f"[{self.remote_addr(ws)}] Error: {type(e)}: {e}")
+            logger.info(f"[{self.remote_addr(ws)}] Gateway {gate_id} connection is broken, closing...")
+        finally:
+            with suppress():
+                self._gates.pop(gate_id, None)
+
+            for associated in gate.associated.values():
+                with suppress():
+                    await associated.close()
+
+            for connection in gate.connections.values():
+                with suppress():
+                    await connection.requestor.close()
+                with suppress():
+                    await connection.acceptor.close()
+            with suppress():
+                await ws.close()
+
+    async def _associate(self, ws, gate_id):
+        """Associates a new control connection with an existing gate."""
+        association_id = None
+        gate = None
+        try:
+            if not (gate := self._gates.get(gate_id)):
+                raise ConnectionError("Invalid gate_id")
+
+            while (association_id := secrets.token_urlsafe(TOKEN_LENGTH)) in gate.associated:
+                pass
+
+            gate.associated[association_id] = ws
+
+            logger.info(f"[{self.remote_addr(ws)}] Creating a new associated connection {association_id} to gate {gate_id}...")
+
+            await ws.send("OK")
+
+            # Awaiting incoming connection requests
+            while True:
+                try:
+                    command = await asyncio.wait_for(ws.recv(), timeout=TOUCH_TIMEOUT)
+                except TimeoutError:
+                    logger.debug(f"[{self.remote_addr(ws)}] Idle associated connection {association_id} for {gate_id}...")
+                    await ws.send("TOUCH")
+                    continue
+
+                if command == "TOUCH":
+                    continue
+
+                if command != "CONNECT":
+                    raise ConnectionError("Invalid request")
+
+                while (connection_id := secrets.token_urlsafe(TOKEN_LENGTH)) in gate.connections:
+                    pass
+
+                connection = Connection(
+                    id=connection_id,
+                    created_at=int(time.time()),
+                    associated_id=association_id,
+                    requestor_secret=secrets.token_urlsafe(TOKEN_LENGTH),
+                    acceptor_secret=secrets.token_urlsafe(TOKEN_LENGTH),
+                    requestor=None,
+                    acceptor=None
+                )
+
+                gate.connections[connection_id] = connection
+
+                logger.info(f"[{self.remote_addr(ws)}] Created a new connection {connection_id} "
+                            f"for gate {gate_id} associated with {association_id}")
+
+                await ws.send(f"CONNECTION {connection_id} {connection.requestor_secret}")
+
+                # We don't want to deal with gate connection errors here
+                with suppress():
+                    await gate.control.send(f"CONNECTION {connection_id} {connection.acceptor_secret}")
+        except ConnectionClosed:
+            logger.info(
+                f"[{self.remote_addr(ws)}] Associated connection {association_id} for {gate_id} connection is closed, cleaning up...")
+        except Exception as e:
+            logger.debug(f"[{self.remote_addr(ws)}] Error: {type(e)}: {e}")
+            logger.info(f"[{self.remote_addr(ws)}] Gateway {gate_id} connection is broken, closing...")
+        finally:
+            with suppress():
+                await ws.close()
+            if gate:
+                to_clean = [gate.connections.pop(c.id) for c in list(gate.connections.values()) if c.associated_id == association_id]
+                for connection in to_clean:
+                    with suppress():
+                        await connection.requestor.close()
+                    with suppress():
+                        await connection.acceptor.close()
+
+    async def _connect(self, ws: WebSocketServerProtocol, gate_id: str, side: str, connection_id: str, secret: str):
+        """Creates a new data connection."""
+        connection = None
+        gate = None
+        try:
+            if not (gate := self._gates.get(gate_id)):
+                raise ConnectionError("Invalid gate")
+
+            if not (connection := gate.connections.get(connection_id)):
+                raise ConnectionError("Invalid gate")
+
+            if all((connection.requestor, connection.acceptor)):
+                raise AlreadyEstablishedConnectionError
+
+            logger.info(f"[{self.remote_addr(ws)}] Creating a new data connection {connection_id} for "
+                        f"gate {gate_id} associated with {connection.associated_id}...")
+
+            required_secret = connection.requestor_secret if side == "requestor" else connection.acceptor_secret
+
+            if not secrets.compare_digest(required_secret, secret):
+                raise AuthenticationError
+
+            # Assign the connection to the side
+            setattr(connection, side, ws)
+
+            async with asyncio.timeout(CONNECTION_TIMEOUT):
+                while not all((connection.requestor, connection.acceptor)):
+                    logger.debug(f"[{self.remote_addr(ws)}] Waiting for the data connection {connection_id} to be established...")
+                    await asyncio.sleep(1)
+
+            logger.info(f"Established data connection {connection_id} for gate_id: {gate_id} associated with {connection.associated_id}")
+
+            opposite = next(iter(c for c in (connection.requestor, connection.acceptor) if c != ws))
+
+            while True:
+                await opposite.send(await ws.recv())
+
+                try:
+                    data = await asyncio.wait_for(ws.recv(), timeout=TOUCH_TIMEOUT)
+                    if not data:
+                        continue
+                    await opposite.send(data)
+                except TimeoutError:
+                    logger.debug(f"[{self.remote_addr(ws)}] Idle data connection {connection_id}...")
+                    # await ws.send(b"")
+                    continue
+        except ConnectionClosed:
+            logger.info(f"[{self.remote_addr(ws)}] Data connection for {gate_id} connection is closed, cleaning up...")
+        except AlreadyEstablishedConnectionError:
+            logger.error(f"[{self.remote_addr(ws)}] Connection {connection_id} already established!")
+            gate = connection = None
+            with suppress():
+                await ws.close(code=CloseCode.POLICY_VIOLATION)
+        except AuthenticationError:
+            logger.error(f"[{self.remote_addr(ws)}] Authentication failed")
+            gate = connection = None
+            with suppress():
+                await ws.close(code=CloseCode.POLICY_VIOLATION)
+        except Exception as e:
+            logger.debug(f"[{self.remote_addr(ws)}] Error: {type(e)}: {e}")
+            logger.info(f"[{self.remote_addr(ws)}] Gateway {gate_id} connection is broken, closing...")
+        finally:
+            if gate:
+                with suppress():
+                    gate.connections.pop(connection.id, None)
+            if connection:
+                with suppress():
+                    await connection.requestor.close()
+                with suppress():
+                    await connection.acceptor.close()
+
+    async def handler(self, ws: WebSocketServerProtocol, path: str):
+        """Parse the path and dispatch the request."""
+
+        client_addr = ":".join(map(str, ws.remote_address))
+        logger.debug(f"New connection from: {client_addr} with path {path}")
+
+        try:
+            action, *splitted = path.removeprefix("/").split("/")
+
+            if action == "connect":
+                gate_id, side, connection_id, secret = splitted
+                if side not in ("requestor", "acceptor"):
+                    raise ValueError
+                await self._connect(ws, gate_id, side, connection_id, secret)
+            elif action == "associate":
+                gate_id, = splitted
+                await self._associate(ws, gate_id)
+            elif action == "create":
+                secret, = splitted
+                await self.auth(ws, secret)
+                await self._create(ws)
+            else:
+                raise ValueError
+        except AuthenticationError:
+            logger.error(f"[{self.remote_addr(ws)}] Authentication failed")
+            await ws.close(code=CloseCode.POLICY_VIOLATION)
+        except ValueError:
+            logger.error(f"[{self.remote_addr(ws)}] Malformed path: {path}")
+            await ws.close(code=CloseCode.INVALID_DATA)
+        except Exception as e:
+            logger.error(f"[{self.remote_addr(ws)}] Unexpected error: {type(e)}: {e}")
+            await ws.close(code=CloseCode.INVALID_DATA)
+
+
+async def server(host: str, port: int, secret: str, single: Optional[str] = None):
+    stop = asyncio.Event()
+
+    loop = asyncio.get_event_loop()
+    loop.add_signal_handler(signal.SIGINT, lambda: stop.set())
+
+    print(secret)
+
+    gp = GatesProvider(secret, single=single)
+    try:
+        async with serve(gp.handler, host=host, port=port):
+            logger.info("Serving on ws://localhost:8000/...")
+            await stop.wait()
+    except OSError as e:
+        logger.error(f"{e}")
+
+    logger.info("Exiting...")
+
+
+def main():
+    parser = ArgumentParser(prog="wultiplexor", description="The websocket connections multiplexor gateway.")
+    parser.add_argument("-n", "--host", default="localhost", help="The host to bind to.")
+    parser.add_argument("-p", "--port", default=8000, type=int, help="The port to bind to.")
+    parser.add_argument("-s", "--secret", default="gate", help="The secret to use for authentication.")
+
+    parser.add_argument("--single", help="Limit the server to a single gateway.")
+
+    args = parser.parse_args()
+
+    asyncio.run(server(**vars(args)))
+
+
+if __name__ == "__main__":
+    main()
