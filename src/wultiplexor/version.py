26d137c4042acaf5f7021b8261abec336e694447
vd <33198864+vd2org@users.noreply.github.com>
2024-02-20 22:03:39 +0400

    Fixed C.

--- src/wultiplexor/version.py
+++ src/wultiplexor/version.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2024 by Ivan.
+# Copyright (C) 2024 by Vd <wultiplexor@vd2.org>.
 # This file is part of Wultiplexor project.
 # Wultiplexor is released under the MIT License (see LICENSE).
 
--- src/wultiplexor/wultiplexor.py
+++ src/wultiplexor/wultiplexor.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2024 by Vd.
+# Copyright (C) 2024 by Vd <wultiplexor@vd2.org>.
 # This file is part of Wultiplexor project.
 # Wultiplexor is released under the MIT License (see LICENSE).
 
